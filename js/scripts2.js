818aa1563f14e6182f07817f7d548212001c863d
Allison Sadin <aesadin@gmail.com>
2020-06-03 12:01:03 -0700

    fixed bug in span id and label that was preventing display of the id story
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

---
+++ js/scripts2.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const numberInput = $("input#number").val();
+    const nameInput = $("input#name").val();
+    const pronounInput = $("input#pronoun").val();
+    const nounInput = $("input#noun").val();
+    const activityInput = $("input#activity").val();
+    const verb2Input = $("input#verb2").val();
+    const adjectiveInput = $("input#adjective").val();
+
+    $("#numberspan").text(numberInput);
+    $("#namespan").text(nameInput);
+    $("#pronounspan").text(pronounInput);
+    $("#nounspan").text(nounInput);
+    $("#activityspan").text(activityInput);
+    $("#verb2span").text(verb2Input);
+    $("#adjectivespan").text(adjectiveInput);
+    $("#storyspan").show();
+
+    $("#story").show();
+
+    event.preventDefault();
+    
+  });
+});
